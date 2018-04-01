17dea7b56446b77b1117def500baf55c41870bd2
PS Vishnu <ps.vishnu212@gmail.com>
2018-04-01 00:08:09 -0800

    Initialise 30 days

---
+++ 30 Days of code/DataTypes.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+        int j; double e;String t;
+        Scanner scan = new Scanner(System.in);
+
+        j=scan.nextInt();
+        e=scan.nextDouble();
+        scan.nextLine();
+        t=scan.nextLine();
+        System.out.println(i+j);
+        System.out.println(d+e);
+        System.out.println(s+t);
+        scan.close();
+}
+}
