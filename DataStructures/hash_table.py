f965d67ac8aa27236da96a162442098c66c04d92
PS Vishnu <psvishnu.it13@gmail.com>
2021-07-01 18:31:42 +0530

    Update hash_table.py

--- DataStructures/hash_table.py
+++ DataStructures/hash_table.py
@@ -1,2 +1,31 @@
 class HashTable:
-    pass
+    """
+    Insertion O(1)
+    Lookup O(1)
+
+    Two methods to prevent collision:
+    1. Chaining
+    2. Linear probing
+    """
+    def __init__(self):
+        self.size = 1024
+        self.index_list = [None] * self.size
+
+    def get_index(self, value):
+        hash_value = hash(value)
+
+    def __getitem__(self, key):
+        pass
+
+    def __setitem__(self, key, value):
+        pass
+
+
+
+if __name__ == "__main__":
+    ht = HashTable()
+
+    ht.put("a", 1)
+    print(ht["a"])
+
+    # ht.get("a")
