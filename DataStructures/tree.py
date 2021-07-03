444dc7db7e89c8948e59b7a2ad84e9ce002566b5
PS Vishnu <psvishnu.it13@gmail.com>
2021-07-03 15:27:12 +0000

    Complete BST deletion

--- DataStructures/tree.py
+++ DataStructures/tree.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 class BSTNode:
     def __init__(self, data, level=0):
         self.data = data
@@ -19,7 +20,7 @@ class BSTNode:
             self.left.print()
         if self.right is not None:
             self.right.print()
-    
+
     def insert(self, value):
         if value < self.data:
             if self.left is None:
@@ -46,20 +47,43 @@ class BSTNode:
 
     def find_max(self):
         if self.right is not None:
-            self.right.find_max()
+            return self.right.find_max()
         return self.data
 
-    def find_min(self, value):
+    def find_min(self):
         if self.left is not None:
-            self.left.find_min()
+            return self.left.find_min()
         return self.data
-    
+
     def delete(self, value):
-        pass
+        """
+        - Leaf node
+        - not a leaf or root: only left child
+        - not a leaf or root: only right child
+        - not a leaf or root: both right and left children
+        - single element tree: found
+        - single element tree: not found
+        - Root node 
+        """
+        if value < self.data and self.left is not None:
+            self.left = self.left.delete(value)
+        elif value > self.data and self.right is not None:
+            self.right = self.right.delete(value)
+        else:
+            if (self.left is None) and (self.right is None):
+                return None
+            if self.right is None:
+                return self.left
+            if self.left is None:
+                return self.right
+            min_subnode = self.right.find_min()  # Alternate: switch with left max
+            self.data = min_subnode
+            self.right = self.right.delete(min_subnode)
+        return self
 
 
 class BinarySearchTree:
-    def __init__(self, values:List):
+    def __init__(self, values: List):
         self.root = BSTNode(values[0])
         for value in values[1:]:
             self.insert(value)
@@ -67,7 +91,17 @@ class BinarySearchTree:
     def insert(self, value):
         self.root.insert(value)
 
-if __name__ == "__main__":
-    tree = BinarySearchTree([10,5,3,56,23,5])
+    def delete(self, value):
+        return self.root.delete(value)
 
-    print(tree.root.print())
+
+if __name__ == "__main__":
+    tree = BinarySearchTree([10, 5, 3, 56, 23, 5, 59])
+
+    tree.root.print()
+
+    print(tree.root.find_max())
+    print(tree.root.find_min())
+
+    tree.delete(23)
+    tree.root.print()
